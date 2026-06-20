709cd6551f35ec337cee8ab4cfa6fe3870f43c32
gilbertmarin0820 <gilbertmarin0820@gmail.com>
2026-06-20 15:29:43 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -13,17 +13,17 @@ const CACHE_NAME = 'gilbert-expenses-v1';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
-  './icons/icon-72.png',
-  './icons/icon-96.png',
-  './icons/icon-128.png',
-  './icons/icon-144.png',
-  './icons/icon-152.png',
-  './icons/icon-192.png',
-  './icons/icon-384.png',
-  './icons/icon-512.png',
-  './icons/icon-maskable-192.png',
-  './icons/icon-maskable-512.png',
-  './icons/apple-touch-icon.png',
+  './icon-72.png',
+  './icon-96.png',
+  './icon-128.png',
+  './icon-144.png',
+  './icon-152.png',
+  './icon-192.png',
+  './icon-384.png',
+  './icon-512.png',
+  './icon-maskable-192.png',
+  './icon-maskable-512.png',
+  './apple-touch-icon.png',
 ];
 
 // Hosts whose responses should NEVER be cached (always go to network).
